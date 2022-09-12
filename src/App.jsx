96fc793f566d9574e2cfe438504220c36a9352d0
abdallamansour <abdallamansour@gmail.com>
2022-09-12 20:47:05 +0200

    Upload Files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { BrowserRouter, Routes} from "react-router-dom";
+import React from "react";
+
+// import Index from "@/pages/index";
+// import CryptoCurrencyPage from "@/pages/CryptoCurrencyPage";
+// import Error404 from "@/pages/Error404";
+import Index from "./pages/index"
+
+function App() {
+  return (
+    <Index />
+  );
+}
+
+export default App;
